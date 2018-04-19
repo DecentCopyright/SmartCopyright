9a2ca6cea47fa0f40933678a09cf4b410f433f62
Jungho Bang <me@bangtoven.com>
2018-04-19 00:36:03 -0400

    fix bugs, write abi, get contract instance.

--- deploy.py
+++ deploy.py
@@ -1,34 +1,36 @@
 import json
 import web3
 
-from web3 import Web3, TestRPCProvider
-from solc import compile_source
+from web3 import Web3, EthereumTesterProvider
+from solc import compile_files
 from web3.contract import ConciseContract
 
-# Solidity source code
-file = open("contracts/Copyright.sol", "r")
-contract_source_code = file.read()
-
-compiled_sol = compile_source(contract_source_code) # Compiled source code
-contract_interface = compiled_sol['<stdin>:Copyright']
+compiled_sol = compile_files(['contracts/Copyright.sol']) # Compiled source code
+contract_interface = compiled_sol['contracts/Copyright.sol:Copyright']
 
 # web3.py instance
-w3 = Web3(TestRPCProvider())
-
 # Instantiate and deploy contract
+w3 = Web3(EthereumTesterProvider())
 contract = w3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
 
+with open('copyring.abi', 'w') as outfile:
+    print('Writing abi file...')
+    json.dump(contract_interface['abi'], outfile)
 
 # Get transaction hash from deployed contract
-tx_hash = contract.constructor().transact({'from': w3.eth.accounts[0]}) #contract.deploy(transaction={'from': w3.eth.accounts[0], 'gas': 520000})
-
 # Get tx receipt to get contract address
+tx_hash = contract.constructor().transact({'from': w3.eth.accounts[0]}) 
 tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
 contract_address = tx_receipt['contractAddress']
 print('Contract address: {}'.format(contract_address))
 
+
 # Contract instance in concise mode
-contract_instance = w3.eth.contract(abi=contract_interface['abi'], address=contract_address, ContractFactoryClass=ConciseContract)
+abifile = open('copyring.abi', 'r')
+abi = json.load(abifile)
+abifile.close()
+
+contract_instance = w3.eth.contract(abi=abi, address=contract_address, ContractFactoryClass=ConciseContract)
 
 # Getters + Setters for web3.eth.contract object
 # print('Contract value: {}'.format(contract_instance.greet()))
