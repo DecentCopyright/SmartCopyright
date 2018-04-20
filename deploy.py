2efd51c0b710173365d3e4f8c4b66d0cb850a94b
Jungho Bang <me@bangtoven.com>
2018-04-20 19:32:23 -0400

    code to deploy

--- deploy.py
+++ deploy.py
@@ -2,15 +2,6 @@ import json
 import web3
 from web3 import Web3, EthereumTesterProvider
 from solc import compile_files
-from web3.contract import ConciseContract
-
-import argparse
-import os
-import tarfile
-import random
-import string
-import ipfsapi
-# from simplecrypt import encrypt, decrypt
 
 compiled_sol = compile_files(['contracts/Copyright.sol']) # Compiled source code
 contract_interface = compiled_sol['contracts/Copyright.sol:Copyright']
@@ -20,10 +11,6 @@ contract_interface = compiled_sol['contracts/Copyright.sol:Copyright']
 w3 = Web3(EthereumTesterProvider())
 contract = w3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
 
-with open('copyring.abi', 'w') as outfile:
-    print('Writing abi file...')
-    json.dump(contract_interface['abi'], outfile)
-
 # Get transaction hash from deployed contract
 # Get tx receipt to get contract address
 tx_hash = contract.constructor().transact({'from': w3.eth.accounts[0]})
@@ -31,3 +18,11 @@ tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
 contract_address = tx_receipt['contractAddress']
 print('Contract address: {}'.format(contract_address))
 print('Deployed!\n')
+
+deployed_result = {}
+deployed_result['contract_address'] = contract_address
+deployed_result['abi'] = contract_interface['abi']
+
+with open('smart-copyright.info', 'w') as outfile:
+    print('Writing deploy info file...')
+    json.dump(deployed_result, outfile)
--- test.py
+++
@@ -1,9 +0,0 @@
-def main():
-    trans = Transaction()from transactions import Transaction
-
-
-abi_address = 'copyright.abi'
-contract_address = '0xF2E246BB76DF876Cef8b38ae84130F4F55De395b'
-account_address = 238223
-
-t = new Transaction(abi_address, contract_address, account_address)
